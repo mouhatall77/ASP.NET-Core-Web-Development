e8ac049b581d12d5a22c6e9b302e17acdac7aa11
mouhatall77 <56361929+mouhatall77@users.noreply.github.com>
2021-06-22 01:33:49 -0400

    Web App that display list of products
    C# Core Web Application Activity 3a Database app with Mockaroo Data
    
    We build a C# Web application in Visual Studio that performs database
    operations Create Read Update and Delete.
    In this example we build a databse and fill it with fake data form Mockaroo.
    We used Faker which is a fake data generator for .NET
    Created a static list of 104 items to be displayed in ./product

---
+++ AppointmentMaker/AppointmentMaker/Controllers/AppointmentController.cs
@@ -0,0 +1,29 @@
+﻿using AppointmentMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentMaker.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        static List<AppointmentModel> appointments = new List<AppointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details(AppointmentModel appointment)
+        {
+            appointments.Add(appointment);
+            return View("Details", appointment);
+        }
+    }
+}
--- AppointmentMaker/AppointmentMaker/Models/AppointmentModel.cs
+++ AppointmentMaker/AppointmentMaker/Models/AppointmentModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +9,24 @@ namespace AppointmentMaker.Models
 {
     public class AppointmentModel
     {
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        [DisplayName("Patient's full name")]
         public string PatientName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Appointment Request Date")]
         public DateTime AppointmentDate { get; set; }
-        public decimal PatientWorth { get; set; }
+
+        [DataType(DataType.Currency)]
+        [DisplayName("Patient's Approximate Net Worth")]
+        public decimal PatientNetWorth { get; set; }
+        [DisplayName("Patient's Doctor's Last Name")]
         public string DoctorName { get; set; }
-        public int PatientLevel { get; set; }
+
+        [Range(1,10)]
+        [DisplayName("Patient's Perceived Level of pain (1 low to 10 high")]
+        public int PainLevel { get; set; }
 
     }
 }
