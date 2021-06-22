19fd4af71274aa2529754efef0e207e343ddb453
mouhatall77 <56361929+mouhatall77@users.noreply.github.com>
2021-06-22 01:40:23 -0400

    Web app Static list of products
    C# Core Web Application Activity 3a Database app with Mockaroo Data
    
    We build a C# Web application in Visual Studio that performs database
    operations Create Read Update and Delete.
    In this example we build a databse and fill it with fake data form Mockaroo.
    We used Faker which is a fake data generator for .NET
    Created a static list of 104 items to be displayed in ./product

---
+++ Activity2/Activity2/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Activity2.Models;
+using Activity2.Services;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            HardCodedSampleDataRepository hardCodedSampleDataRepository = new HardCodedSampleDataRepository();
+
+            return View(hardCodedSampleDataRepository.GetAllProducts());
+        }
+
+        public IActionResult Message()
+        {
+            return View("message");
+        }
+
+        public IActionResult Welcome(string name, int secretNumber = 13)
+        {
+            ViewBag.Name = name;
+            ViewBag.Secret = secretNumber;
+            return View();
+        }
+    }
+}
